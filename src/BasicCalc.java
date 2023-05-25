35212747745b0b4772bb85047d79008196d6f881
manu51069 <manubs51069@gmail.com>
2023-05-25 12:26:15 +0530

    New Project-BasicCalc

---
+++ src/BasicCalc.java
@@ -0,0 +1,19 @@
+
+public class BasicCalc {
+	static void add(int a,int b) {
+		System.out.println(a+b);
+	}
+	static void multi(int a,int b) {
+		System.out.println(a*b);
+	}
+	static void div(int a,int b) {
+		System.out.println(a/b);
+	}
+
+	public static void main(String[] args) {
+		add(10,20);
+		multi(10,20);
+		div(20,10);
+	}
+
+}
